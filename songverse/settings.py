39385b733820bc982310be3ed6b1b782e5a48d0d
kko3ch <tom.k.kipkurui@gmail.com>
2026-07-19 15:06:07 +0300

    Fix Neon SSL disconnect by disabling persistent DB connections

--- songverse/settings.py
+++ songverse/settings.py
@@ -90,7 +90,8 @@ WSGI_APPLICATION = 'songverse.wsgi.application'
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+        conn_max_age=0,
+        conn_health_checks=True,
     )
 }
 
